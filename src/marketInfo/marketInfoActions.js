1ee5d14dcba04d10c45cafd43cc9babb539ef884
ribas9521 <guilherme.rifer@gmail.com>
2018-06-07 10:18:56 -0300

    working with new version of redux-promise

--- src/marketInfo/marketInfoActions.js
+++ src/marketInfo/marketInfoActions.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 const BASE_URL = 'https://api.coinmarketcap.com/v2/ticker/'
 
-
 export function getMarketInfo(){
-    const request = axios.get(`${BASE_URL}?limit=0`)
+    const request =  axios.get(`${BASE_URL}?limit=0`)
     return{
         type: 'MARKET_INFO_FETCHED',
         payload: request
